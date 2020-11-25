948b23bc8ca8c1618d6ebd2db21eebdaa3bad19d
상준 <@DESKTOP-D0FO1B0>
2020-11-25 19:46:12 +0900

    Update : Printing a virtual calendar

---
+++ src/honux/calendar/Prompt.java
@@ -0,0 +1,33 @@
+package honux.calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	private final static String PROMPT = "cal> ";
+
+	public void runPrompt() {
+
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+		int month = 1;
+		while (month != -1) {
+			System.out.println("달을 입력하세요");
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if (month == -1 || month > 12) {
+				continue;
+			}
+			cal.printCalendar(2020 , month);
+		}
+		scanner.close();
+		System.out.println("Bye~");
+	}
+
+	public static void main(String[] args) {
+		// 셸 실행
+		Prompt p = new Prompt();
+		p.runPrompt();
+
+	}
+}
